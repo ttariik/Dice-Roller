258e051c9324ecfee4669923aadb8cfa7fafa8dc
ttariik <tarik.sabanovic2@icloud.com>
2025-08-01 17:12:05 +0200

    .

--- src/app.py
+++ src/app.py
@@ -1,7 +1,17 @@
 from flask import Flask
+from markupsafe import escape
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def hello_world() -> str:
     return "<p>Hello, World!</p>"
+
+@app.route("/<name>")
+def personalized_hello(name: str) -> str:
+    safe_name = escape(name)
+    return f"Hello, {safe_name}"
+
+@app.route("/dices")
+def show_dices() -> str:
+    return "Look at these beautiful dices"
