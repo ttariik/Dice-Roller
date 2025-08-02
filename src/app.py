ea4e7dc766e3822c7446b7a90038c66ca65606e4
ttariik <tarik.sabanovic2@icloud.com>
2025-08-02 16:58:50 +0200

    .

--- src/app.py
+++ src/app.py
@@ -3,12 +3,10 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world() -> str:
-    return render_template("index.html")
+@app.route("/<string:name>")
+def hello_world(name: str = None) -> str:
+    return render_template("index.html" , _name=name)
 
-@app.route("/<name>")
-def personalized_hello(name: str) -> str:
-    return render_template("index.html", _name=name)
 
 @app.route("/dices")
 def show_dices() -> str:
